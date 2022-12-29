f9c7797d83a766e1206d545429811ef4efdc0b25
Lucas Anderson Ladislau Aguiar <laanderson183@gmail.com>
2022-12-29 10:49:35 -0400

    adaptando o js para arrow function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,8 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 fetch(url)
-    .then(function (responde){
-        return responde.json()
-}).then( function (jsonBody){
-    console.log(jsonBody)
-}).catch(function(error){
-    console.log(error);
-})
+    .then((responde) => responde.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
 
 console.log(20)
