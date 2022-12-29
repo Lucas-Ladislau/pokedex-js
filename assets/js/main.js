3700c99035fcc8639f73f7a4bfd0d83fb0f0a340
Lucas Anderson Ladislau Aguiar <laanderson183@gmail.com>
2022-12-29 10:40:59 -0400

    requisição da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+    .then(function (responde){
+        return responde.json()
+}).then( function (jsonBody){
+    console.log(jsonBody)
+}).catch(function(error){
+    console.log(error);
+})
+
+console.log(20)
